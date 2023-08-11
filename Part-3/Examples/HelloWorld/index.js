9649cccb47c2f82d3d04fdfe0940d564e5419bee
Catrovitch <ale.engelhardt@outlook.com>
2023-08-11 15:07:48 +0300

    3.7: Phonebook backend step7

--- Part-3/Examples/HelloWorld/index.js
+++ Part-3/Examples/HelloWorld/index.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+app.use(requestLogger)
 
 let notes = [
   {
@@ -40,13 +51,27 @@ app.get('/api/notes/:id', (request, response) => {
   }
 })
 
-app.post('/api/notes', (request, response) => {
+const generateId = () => {
   const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id)) 
+    ? Math.max(...notes.map(n => n.id))
     : 0
+  return maxId + 1
+}
 
-  const note = request.body
-  note.id = maxId + 1
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
 
   notes = notes.concat(note)
 
@@ -57,3 +82,9 @@ const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
