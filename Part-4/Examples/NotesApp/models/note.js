26f29a955d98a8037e1a1e2d57287ec62ac5b5cb
Catrovitch <ale.engelhardt@outlook.com>
2023-08-27 16:53:10 +0300

    part 4 examples

--- Part-4/Examples/NotesApp/models/note.js
+++ Part-4/Examples/NotesApp/models/note.js
@@ -1,30 +1,18 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  important: Boolean,
 
-
-const url = process.env.MONGODB_URI
-
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
-  const noteSchema = new mongoose.Schema({
-    content: {
-      type: String,
-      minLength: 5,
-      required: true
-    },
-    important: Boolean
-  })
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
+})
 
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
@@ -35,4 +23,4 @@ noteSchema.set('toJSON', {
 })
 
 
-module.exports = mongoose.model('Note', noteSchema) 
+module.exports = mongoose.model('Note', noteSchema)
